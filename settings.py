053ce24654bd652f659cd6f00d069fd3741a12d1
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2014-04-22 12:27:53 -0400

    Switch to the Bootstrap Molokai Theme

--- settings.py
+++ settings.py
@@ -243,6 +243,7 @@ TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 ################
 
 INSTALLED_APPS = (
+    "sleepanarchy",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
