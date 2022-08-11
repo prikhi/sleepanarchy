ed56b7189af3a0d603845aff63eb554ba827379e
Pavan Rikhi <pavan.rikhi@gmail.com>
2022-08-11 10:24:08 -0400

    Remove Old Mezzanine Repo

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
